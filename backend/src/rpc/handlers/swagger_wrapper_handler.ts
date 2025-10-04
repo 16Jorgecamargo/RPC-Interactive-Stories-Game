3edaa1a1543fd48ff1ecb57cf0599bdc9fde9b68
Jorge Daniel Ristow de Camargo <92612384+16Jorgecamargo@users.noreply.github.com>
2025-10-04 18:33:39 -0300

    mudanças de ambiente

--- backend/src/rpc/handlers/swagger_wrapper_handler.ts
+++
@@ -1,8 +0,0 @@
-import { FastifyPluginAsync } from 'fastify';
-import { registerAllWrappers } from './wrappers/index.js';
-
-const swaggerWrapperHandler: FastifyPluginAsync = async (app) => {
-  await registerAllWrappers(app);
-};
-
-export default swaggerWrapperHandler;
